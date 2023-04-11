e85e46e11e6e78b9bfd67ce74f30dfdb38eb9332
Abdul rehman <official.mani168@gmail.com>
2023-04-12 02:06:27 +0500

    third project

---
+++ index.js
@@ -0,0 +1,72 @@
+var selectedRow = null
+
+function onFormSubmit(e) {
+	event.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
+}
+
+//Retrieve the data
+function readFormData() {
+    var formData = {};
+    formData["name"] = document.getElementById("name").value;
+    formData["work"] = document.getElementById("work").value;
+    formData["time"] = document.getElementById("time").value;
+    formData["day"] = document.getElementById("day").value;
+    return formData;
+}
+
+//Insert the data
+function insertNewRecord(data) {
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.name;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.work;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.time;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.day;
+    cell4 = newRow.insertCell(4);
+        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+}
+
+//Edit the data
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("work").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("time").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("work").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.name;
+    selectedRow.cells[1].innerHTML = formData.work;
+    selectedRow.cells[2].innerHTML = formData.time;
+    selectedRow.cells[3].innerHTML = formData.day;
+}
+
+//Delete the data
+function onDelete(td) {
+    if (confirm('Do you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+//Reset the data
+function resetForm() {
+    document.getElementById("name").value = '';
+    document.getElementById("work").value = '';
+    document.getElementById("time").value = '';
+    document.getElementById("day").value = '';
+    selectedRow = null;
+}
